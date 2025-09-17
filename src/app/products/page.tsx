38ccbb7e3aaef0afaefc5d302e979a35eb9d7988
Rohilla <lakshay.rohilla@sms-group.com>
2025-09-17 10:23:30 +0530

    Routing Dynamic-Routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+const Products = () => {
+    return (
+        <>
+            <h1>Products List !!</h1>
+            <p>Product 1</p>
+            <p>Product 2</p>
+            <p>Product 3</p>
+        </>  
+    )
+}
+
+export default Products;
